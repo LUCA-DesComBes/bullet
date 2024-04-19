aef39c2b559a40b7e4a57c48fe0ba1031ee5e3c0
LUCA-DesComBes <ldescombes@edenschool.fr>
2024-04-19 14:08:08 +0200

    project finished

--- main.js
+++ main.js
@@ -4,24 +4,25 @@ const inputRange = document.querySelector("input");
 
 let rdm = Math.floor(Math.random() * 101)
 console.log(rdm);
+let rdmParagraphe = document.getElementById("rdmPara");
+rdmParagraphe.textContent = rdm;
 inputRange.addEventListener("input", (e)=>{
     let random = parseInt(e.target.value)
+    console.log(random);
     let score = 0;
     let scorePara = document.querySelector("#score");
-    let rdmParagraphe = document.getElementById("rdmPara");
-    rdmParagraphe.textContent = random;
     // 27 - 0 = 27 || 0 - 27 = -27
     //si random est === rdm alors ajoutez à scorePara 5 de score 
     if(random === rdm){
        scorePara.textContent = 5
        alert("GG, t'as trouvée le nombre, c'était bien: " + rdm)
     }
-     else if (random - score > 5 || score - random < 5){
-        scorePara.textContent = 2
+    else if(Math.abs(random - rdm) <= 5) {
+            scorePara.textContent = 2;
+    }else if(Math.abs(random - rdm) <= 20){
+            scorePara.textContent = 1;
     }
-    else if(random - score >= 10 || score - random <= 10) {
+    else {
         scorePara.textContent = 0;
-    } else {
-        console.log("error");
     }
 })
